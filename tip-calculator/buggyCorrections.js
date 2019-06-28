89953091c0b7bcf30fb9839d5cc0c0e03a868a01
Adriano Cordeiro Marra <adrianomarra90@gmail.com>
2019-06-28 15:03:18 -0500

    Submit my homework 3 for day 4 and some modifications on the layout

---
+++ tip-calculator/buggyCorrections.js
@@ -0,0 +1,25 @@
+//Buggy 1
+(function gogogo() {
+    alert("Yay, it works!");
+})();
+
+//Buggy 2
+(function gogogo() {
+    alert("Yay, it works!");
+})();
+
+// Buggy 3
+(function gogogo() {
+    alert("Yay, it works!");
+})();
+
+//Buggy 4
+function somethingElse() {
+        if (5 > 2) {
+            alert("Bigger");
+        if (5 < 2) {
+            alert("Smaller");
+        }
+    }
+}
+
