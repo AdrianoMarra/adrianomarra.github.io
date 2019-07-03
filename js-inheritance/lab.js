b9a4f3ac21ad9f0c8bc0445fe36b72797a45cc19
Adriano Marra <adrianomarra90@gmail.com>
2019-07-02 20:57:36 -0500

    Finish Lab 08 about JS inheritance

--- js-inheritance/lab.js
+++ js-inheritance/lab.js
@@ -1,13 +1,16 @@
+/** 
+ * ES5 Function Expressions: 
+*/
 
-//ES5 Function Expressions: 
-(function() {
+let bike = function() {
     'use strict'
 
     let createBicyclePrototye = function() {
 
         let speed = 0;
+        let type = "Normal bike";
 
-        const applyBreak = function (givenSpeed) {
+        const applyBrake = function (givenSpeed) {
             this.speed -=  givenSpeed;
         }
 
@@ -16,8 +19,9 @@
         }
 
         return {
+            type: type,
             speed: speed,
-            applyBreak: applyBreak,
+            applyBrake: applyBrake,
             speedUp: speedUp
         }
     }
@@ -25,51 +29,143 @@
     let createMountainBikeProtoype = function(bicyclePrototype) {
         let create = Object.create(bicyclePrototype);
         create.gear = 1;
+        create.type = "Mountain bike";
         create.setGear = function(gear) {
             this.gear = gear;
         }
         return create;
     }
 
-    let start = function() {
-        let bicyclePrototype = createBicyclePrototye();
-        let mountainBikeProtoype = createMountainBikeProtoype(bicyclePrototype);
-
-        console.log(mountainBikeProtoype.speedUp(2));
-        console.log(mountainBikeProtoype.speed);
-        console.log(mountainBikeProtoype.setGear(3));
-        console.log(mountainBikeProtoype.gear);
-        console.log(bicyclePrototype);
-        console.log(mountainBikeProtoype);
+    return {
+        createBicyclePrototye: createBicyclePrototye, 
+        createMountainBikeProtoype: createMountainBikeProtoype
     }
 
-    start();
+};
 
-})();
 
-// ES6 Class construct: 
-(function(){
+/** 
+ * ES6 Class construct: 
+*/
+class BicyclePrototype {
+    
+    speed = 0;
+    type = "Normal bike from ES6"
 
-    class BicyclePrototype {
-        
-        speed = 0;
-
-        constructor() {
-            this.speed = speed;
-            this.applyBreak = applyBreak;
-            this.speedUp = speedUp;
-        }
-
-        applyBreak = function (givenSpeed) {
-            this.speed -=  givenSpeed;
-        }
-
-        speedUp = function (givenSpeed) {
-            this.speed +=  givenSpeed;
-        }
-
+    applyBrake = function (givenSpeed) {
+        this.speed -=  givenSpeed;
     }
 
-})();
+    speedUp = function (givenSpeed) {
+        this.speed +=  givenSpeed;
+    }
+}
 
-// Constructor function 
+class MountainBikeProtoype extends BicyclePrototype {
+    constructor() {
+        super();
+        this.gear = 2;
+        this.type = "Mountain bike ES6";
+    }
+
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+}
+
+/**
+ * Function construtor:
+ */
+
+function BicyclePrototypeConstructor() {
+
+    type = "Normal bike from FC"
+    speed = 0;
+
+    applyBrake = function (givenSpeed) {
+        this.speed -=  givenSpeed;
+    }
+
+    speedUp = function (givenSpeed) {
+        this.speed +=  givenSpeed;
+    }
+
+    return {
+        type: type,
+        speed: speed,
+        applyBrake: applyBrake,
+        speedUp: speedUp
+    }
+}
+
+function MountainBikeProtoypeConstructor(bicyclePrototype) {
+    let create = Object.create(bicyclePrototype);
+    create.type = "Mountain bike FC";
+    create.gear = 3;
+    
+    setGear = function (newGear) {
+        create.gear = newGear;
+    }
+
+    return create;
+}
+
+/** 
+ * Testing on the screen:
+*/
+let controll = function () {
+    let panel = document.getElementsByTagName("textarea")[0];
+    let transformBtn = document.getElementById("transform");
+    let speedupBtn = document.getElementById("speedup");
+    let brakeBtn = document.getElementById("brake");
+    let timer;
+
+    //Uncomment one of the 3 lines to test the functionality:
+
+    let newBike = new BicyclePrototypeConstructor(); 
+    // let newBike = new BicyclePrototype(); 
+    //let newBike = bike().createBicyclePrototye();
+    
+    transformBtn.onclick = () => transformBike();
+    speedupBtn.onclick = () => bikeSpeedUp();
+    brakeBtn.onclick = () => bikeBrake();
+
+
+    (function () {
+        panel.value = newBike.type + ", speed is: " + newBike.speed + " km/h";
+    })();
+
+    let transformBike = function () {
+
+        //Uncomment one of the 3 lines to test the functionality:
+
+        newBike = new MountainBikeProtoypeConstructor(newBike); 
+        //newBike = new MountainBikeProtoype(); 
+        //newBike = bike().createMountainBikeProtoype(newBike);
+
+        panel.value = newBike.type + ", the gear is: " + newBike.gear +", and speed is: " + newBike.speed + " km/h";
+    }
+
+    let bikeSpeedUp = function () {
+        timer = setInterval(()=> {
+            newBike.speed += 1;
+            panel.value = newBike.type + ", the gear is: " + newBike.gear + ", speed is: " + newBike.speed + " km/h";
+        }, 80);
+    }  
+
+    let bikeBrake = function () {
+        clearInterval(timer);
+        timer = setInterval(()=> {
+            newBike.speed -= 1;
+            panel.value = newBike.type + ", the gear is: " + newBike.gear + ", speed is: " + newBike.speed + " km/h";
+            
+            if (newBike.speed <= 0)                
+                clearInterval(timer);
+
+        }, 40);
+    }
+};
+
+window.onload = function() {
+    controll();
+};
